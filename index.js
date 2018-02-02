943801c66413a0a53d8d4766c3c4fa6caa10a291
David W Jones <davidandlee.jones@gmail.com>
2018-02-02 09:30:58 -0600

    Level 1 completed

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 
 // Get references to the tbody element, input field and button
 var $tbody = document.querySelector("tbody");
-var $stateInput = document.querySelector("#date");
+var $dateInput = document.querySelector("#date");
 var $searchBtn = document.querySelector("#search");
 
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 $searchBtn.addEventListener("click", handleSearchButtonClick);
 
-// Set filteredAddresses to addressData initially
-var filteredAddresses = dataSet;
+// Set filteredData to dataSet initially
+var filteredData = dataSet;
 
-// renderTable renders the filteredAddresses to the tbody
+// renderTable renders the filtered data to the tbody
 function renderTable() {
   $tbody.innerHTML = "";
-  for (var i = 0; i < filteredAddresses.length; i++) {
-    // Get get the current address object and its fields
-    var address = filteredAddresses[i];
-    var fields = Object.keys(address);
+  for (var i = 0; i < filteredData.length; i++) {
+    // Get get the current sighting object and its fields
+    var  sighting= filteredData[i];
+    var fields = Object.keys(sighting);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
+      // For every field in the sighting object, create a new cell at set its inner text to be the current value at the current sighting's field
       var field = fields[j];
       var $cell = $row.insertCell(j);
-      $cell.innerText = address[field];
+      $cell.innerText = sighting[field];
     }
   }
 }
 
 function handleSearchButtonClick() {
-  // Format the user's search by removing leading and trailing whitespace, lowercase the string
-  var filterState = $stateInput.value.trim().toLowerCase();
+  // Format the user's search by removing leading and trailing whitespace
+  var filterDate = $dateInput.value.trim();
 
-  // Set filteredAddresses to an array of all addresses whose "state" matches the filter
-  filteredAddresses = addressData.filter(function(address) {
-    var addressState = address.state.toLowerCase();
+  // Set filteredData to an array of all sightings whose "date" matches the filter
+  filteredData = dataSet.filter(function(sighting) {
+    var sightingDate = sighting.datetime;
 
-    // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
-    return addressState === filterState;
+    // If true, add the sighting to the filteredData, otherwise don't add it to filteredData
+    return sightingDate === filterDate;
   });
   renderTable();
 }
